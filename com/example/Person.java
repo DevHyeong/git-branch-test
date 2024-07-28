810ee3839eeda7e2e542df992b159c9c8bd05268
jo <chojh3123@gmail.com>
2024-07-28 21:30:03 +0900

    feat: person

---
+++ com/example/Person.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Person {
+    private String name;
+    private int age;
+    private Country country;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+}
