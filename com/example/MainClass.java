643f7217a9afcf0c4cf0e50fae28d1724c8b6de3
jo <chojh3123@gmail.com>
2024-07-28 21:18:25 +0900

    init

---
+++ com/example/MainClass.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.machine.Car;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.move();
+    }
+}
---
+++ com/example/machine/Car.java
@@ -0,0 +1,11 @@
+package com.example.machine;
+
+public class Car {
+    private int length;
+    private int weight;
+    private Location location;
+
+    public void move(){
+
+    }
+}
